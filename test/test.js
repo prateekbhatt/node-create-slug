a799187a29b51391feab87e9a2f58d977ba13423
prateekbhatt <prattbhatt@gmail.com>
2013-06-24 11:03:26 +0530

    trim spaces at the start and the end of string

--- test/test.js
+++ test/test.js
@@ -1,13 +1,27 @@
 var assert = require("assert")
-  , slugs = require("../");
+  , makeSlug = require("../");
 
 describe('node-slugs', function(){
 
-  it('should convert string to lower case', function(){
-    assert.equal(slugs('HelloWorld'), 'helloworld');
+  it('should convert numbers to string', function(){
+    var slug = makeSlug(4242);
+    assert.equal(slug, '4242');
   });
 
-  it('should replace space with hyphen', function(){
-    assert.equal(slugs('hello  world'), 'hello-world');
+  it('should convert string to lower case', function(){
+    assert.equal(makeSlug('HeLLoWorld'), 'helloworld');
   });
+
+  it('should trim any spaces at the beginning of the string', function(){
+    assert.equal(makeSlug('   hello-world'), 'hello-world');
+  });
+
+  it('should trim any spaces at the end of the string', function(){
+    assert.equal(makeSlug('hello-world    '), 'hello-world');
+  });
+
+  it('should replace spaces in the middle with hyphens', function(){
+    assert.equal(makeSlug('hello  world    4242'), 'hello-world-4242');
+  });
+
 });

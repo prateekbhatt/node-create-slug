7b681b8f9efd322154c3c326f53fffdb8a3236b0
prateekbhatt <prattbhatt@gmail.com>
2013-06-24 15:34:44 +0530

    added shouldjs for tests

--- test/test.js
+++ test/test.js
@@ -1,27 +1,26 @@
-var assert = require("assert")
+var should = require("should")
   , makeSlug = require("../");
 
 describe('node-slugs', function(){
 
   it('should convert numbers to string', function(){
-    var slug = makeSlug(4242);
-    assert.equal(slug, '4242');
+    should.equal(makeSlug(4242), '4242');
   });
 
   it('should convert string to lower case', function(){
-    assert.equal(makeSlug('HeLLoWorld'), 'helloworld');
+    should.equal(makeSlug('HeLLoWorld'), 'helloworld');
   });
 
   it('should trim any spaces at the beginning of the string', function(){
-    assert.equal(makeSlug('   hello-world'), 'hello-world');
+    should.equal(makeSlug('   hello-world'), 'hello-world');
   });
 
   it('should trim any spaces at the end of the string', function(){
-    assert.equal(makeSlug('hello-world    '), 'hello-world');
+    should.equal(makeSlug('hello-world    '), 'hello-world');
   });
 
   it('should replace spaces in the middle with hyphens', function(){
-    assert.equal(makeSlug('hello  world    4242'), 'hello-world-4242');
+    should.equal(makeSlug('hello  world    4242'), 'hello-world-4242');
   });
 
 });
